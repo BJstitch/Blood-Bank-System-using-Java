11d0de01d4e03addfb398fcd6d80aac50aaeaa33
BJstitch <124802038+BJstitch@users.noreply.github.com>
2023-12-13 16:46:42 +0300

    Add files via upload

---
+++ Hospital.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bloodbanksystem;
+
+public class Hospital extends User {
+   
+    
+    public Hospital(String username, String password) {
+        super(username, password);
+     }
+     
+    public void hospitalOperation(){
+          System.out.println("Hospital operation performed.");
+    }
+    
+    
+     
+}
